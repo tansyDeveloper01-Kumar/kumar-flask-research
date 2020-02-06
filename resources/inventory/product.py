126b97afaca2f730b1c693dbd657b04c772a818b
tansyDeveloper01 <gundla.kumar@tansycloud.com>
2020-02-06 15:43:12 +0530

    product update

--- resources/inventory/product.py
+++ resources/inventory/product.py
@@ -78,4 +78,47 @@ class InvProduct(Resource):
             
         except Exception as e:
             return {'Error': str(e)}, 400
+
         
+    def put(self):
+        try:
+            data = request.get_json()
+            token = request.headers.get('token')
+            
+            sproc_result_array, result_args = is_token_valid(token)
+            client_db_details = [sproc_result for sproc_result in sproc_result_array[0]]
+            
+            client_db_connection = connect_to_database(user=client_db_details[2], 
+                                                       password=client_db_details[3],
+                                                       database=client_db_details[1],
+                                                       host=client_db_details[4])
+
+            
+            input_params = [data.get('entity_id'), data.get('isactive'), data.get('product_type_id'), 
+                            data.get('product_name'), data.get('brand_id'),data.get('uom_type_id'),
+                            data.get('unit_content'), data.get('product_dimensions'),
+                            data.get('isreturnable_item'), data.get('selling_price'), 
+                            data.get('ledger_account_entity_id'),data.get('maintain_inventory_flag'),
+                            data.get('opening_stock'), data.get('re_order_level'), 
+                            data.get('start_reminder_months'),data.get('stop_reminder_months'), 
+                            data.get('max_reminder_count'), data.get('reminder_sms_text'),
+                            data.get('reminder_email_text')]
+
+            result_args, cursor = call_stored_procedure(client_db_connection, 
+                                                        'sproc_inv_product_dml_upd', 
+                                                        *input_params,
+                                                        request.headers.get('session_id'),
+                                                        request.headers.get('user_id'), 
+                                                        40004, 1, 1, 0, 0, 0)
+
+            print('Result args: ', result_args[-3: -2])
+            if result_args[-3: -2][0] is not None:
+                return error_response(code=400, type='Bad Request', 
+                                      message=result_args[-1:][0], 
+                                      fbtrace_id=None), 400
+
+            return {'message': 'Product update successfully'}, 201
+            
+        except Exception as e:
+            return {'Error': str(e)}, 400
+             
