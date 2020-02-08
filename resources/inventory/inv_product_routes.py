2bf510e209ce23bfcc062a1eccdfc84b5989a0df
tansyDeveloper01 <gundla.kumar@tansycloud.com>
2020-02-08 10:43:21 +0530

    product details

--- resources/inventory/inv_product_routes.py
+++ resources/inventory/inv_product_routes.py
@@ -1,6 +1,7 @@
 # ~/mysql-flask-connector/resources/inventory/inv_product_routes.py
 
-from .product import InvProduct
+from .product import InvProduct, InvProductDetails
 
 def inv_product_initialize_routes(api):
     api.add_resource(InvProduct, '/api/v1/product')
+    api.add_resource(InvProductDetails, '/api/v1/product-details')
--- resources/inventory/product.py
+++ resources/inventory/product.py
@@ -11,6 +11,91 @@ from resources.db.procedure import call_stored_procedure, sproc_response, \
                                    error_response
 
 
+class InvProductDetails(Resource):
+    def get(self):
+        try:
+            token = request.headers.get('token')
+            entity_id = int(request.headers.get('entity_id'))
+            sproc_result_array, result_args = is_token_valid(token)
+            client_db_details = [sproc_result for sproc_result in sproc_result_array[0]]
+
+            client_db_connection = connect_to_database(user=client_db_details[2],
+                                                       password=client_db_details[3],
+                                                       database=client_db_details[1],
+                                                       host=client_db_details[4])
+
+            result_args, cursor = call_stored_procedure(client_db_connection,
+                                                        'sproc_inv_product_detail',
+                                                        entity_id,
+                                                        request.headers.get('session_id'),
+                                                        request.headers.get('user_id'),
+                                                        110001, 1, 1, None, None, None)
+
+            sproc_result = sproc_response(cursor)
+
+            if not sproc_result:
+                return error_response(code=404, type='Not Found',
+                                      message='product not found',
+                                      fbtrace_id=None), 404
+
+            product_details = [{'product': each_product[0],
+                                'product_type': each_product[1],
+                                'unit_rate': str(each_product[2]),
+                                'start_reminder_months': each_product[3],
+                                'stop_reminder_months': each_product[4],
+                                'max_reminder_count': each_product[5],
+                                'product_type_entity_id': each_product[6],
+                                'product_entity_id': each_product[7],
+                                'max_reminder_count': each_product[8],
+                                'active': each_product[9],
+                                'reminder_sms_text': each_product[10],
+                                'reminder_email_text': each_product[11],
+                                'uom_type_id': str(each_product[12]),
+                                'unit_content': str(each_product[13]),
+                                're_order_level': each_product[14],
+                                'maintain_inventory_flag': each_product[15]}
+                                for each_product in sproc_result]
+
+            return {'status': 'Success', 'product_details': product_details}, 200
+
+        except Exception as e:
+            return {'Error': str(e)}, 400
+
+
+    def put(self):
+        try:
+            token = request.headers.get('token')
+            entity_id = request.headers.get('entity_id')
+
+            sproc_result_array, result_args = is_token_valid(token)
+            client_db_details = [sproc_result for sproc_result in sproc_result_array[0]]
+
+            client_db_connection = connect_to_database(user=client_db_details[2],
+                                                       password=client_db_details[3],
+                                                       database=client_db_details[1],
+                                                       host=client_db_details[4])
+
+            try:
+                conn = client_db_connection.cursor()
+                conn.callproc('sproc_inv_product_dml_del', (entity_id, 254, 20, 110001, 1, 1, None, None, None))
+                sproc_result = sproc_response(conn)
+
+                if not sproc_result:
+                    return error_response(code=404, type='Not Found',
+                                          message='product not found',
+                                          fbtrace_id=None), 404
+
+                dropdown_data = [{'entity_id': each_product[0], 'deleted_flag': each_product[6]} for each_product in
+                                 sproc_result]
+
+                return {'status': 'Success', 'result': dropdown_data}, 200
+            except Exception as e:
+                return {'status': 'Success', "error": str(e)}, 200
+
+        except Exception as e:
+            return {'Error': str(e)}, 400
+
+
 class InvProduct(Resource):
     def get(self):
         try:
