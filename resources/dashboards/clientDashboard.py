166712cf9f98e458f9177fd06be5e482f34b9d48
tansyDeveloper01 <gundla.kumar@tansycloud.com>
2020-02-10 16:56:33 +0530

    client dashboard

--- resources/dashboards/clientDashboard.py
+++ resources/dashboards/clientDashboard.py
@@ -15,17 +15,26 @@ class OrgClientDashboard(Resource):
     @check_auth_verification()
     def get(self, *args, **kwargs):
         try:
+            data = request.get_json()
+
+            start_date = data.get('start_date')
+            end_date = data.get('end_date')
+            client_entity_id = int(data.get('client_entity_id'))
+
+            iparams = [start_date, end_date, client_entity_id,
+                       request.headers.get('session_id'),
+                       request.headers.get('user_id'), 110001, 1, 1]
+
+            oparams = [None, None, None, None,None, None, None, None,None,None,None]
+
             result_args, cursor = call_stored_procedure(kwargs['client_db_connection'], 
                                                     'sproc_org_rpt_dashboard_client',
-                                                    '2019-12-10','2019-12-10',84,
-                                                    request.headers.get('session_id'), 
-                                                    request.headers.get('user_id'),
-                                                    110001, 1, 1, None, None, None, None,None, None, None, None,None,None,None)
+                                                    *iparams, *oparams)
             
             sproc_result = []
             for result in cursor.stored_results():
                 sproc_result.append(result.fetchall())
-
+                        
             return {'status': 'Success', 'brands': sproc_result}, 200
             
         except Exception as e:
