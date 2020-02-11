02c62ea87e93072e9b55a15ac27b00f728000e05
tansyDeveloper01 <gundla.kumar@tansycloud.com>
2020-02-11 15:36:08 +0530

    login

--- resources/authenticate/auth.py
+++ resources/authenticate/auth.py
@@ -12,47 +12,53 @@ class AuthBackend(Resource):
     def get(self):
         try:
             data = request.get_json()
-            user_id = data.get('user_id')
+            user_id = data.get('domain_name')
             password = data.get('password')
             login_id, domain_name = user_id.split('@', 1)
             client_db_details, result_args = get_client_details_from_master_db(user_domain_name=domain_name)
             
-            client_db_details = client_db_details[0]
-            if client_db_details[0] is None:
-                return error_response(code=404, type='Not found', message='Invalid login', fbtrace_id=None), 404
-            if not client_db_details:
-                return error_response(code=404, type='Not Found', message='Client database not found', fbtrace_id=None), 404
-            token = client_db_details[0]
-            client_db_connection = connect_to_database(user=client_db_details[2], 
-                                                    password=client_db_details[3],
-                                                    database=client_db_details[1],
-                                                    host=client_db_details[4])
-            output_params = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-            
-            result_args, cursor = call_stored_procedure(client_db_connection, 'sproc_sec_login_v2', login_id, password,
-                                                        request.remote_addr, "desktop", *output_params)
-            required_output_params = result_args[4:8]
-            error_params = result_args[-5:]
-            result_sets = sproc_response(cursor)
-            
-            if 0 in error_params:
-                return error_response(code=406, type='Not Acceptable', message='Server timed out', fbtrace_id=None), 406
-            if result_sets is None:
-                return error_response(code=406, type='Not Acceptable', message='Password is incorrect',
-                                    fbtrace_id=None), 406
-            
-            result_sets_array = [result_set[0] for result_set in result_sets]
-            
-            close_connection(client_db_connection, cursor)
-            result_json = {
-                'token': token,
-                'audit_screen_visit': required_output_params[0],
-                'debug_sproc': required_output_params[1],
-                'session_id': required_output_params[2],
-                'user_id': required_output_params[3],
-                'module_names': result_sets_array
-            }
-            return result_json
+            if result_args[5] != None:
+                return {'status': 'Failure', 'message': result_args[5] }, 400
+            else:
+                client_db_details = client_db_details[0]
+                if client_db_details[0] is None:
+                    return error_response(code=404, type='Not found', message='Invalid login', fbtrace_id=None), 404
+                if not client_db_details:
+                    return error_response(code=404, type='Not Found', message='Client database not found', fbtrace_id=None), 404
+                token = client_db_details[0]
+                client_db_connection = connect_to_database(user=client_db_details[2], 
+                                                           password=client_db_details[3],
+                                                           database=client_db_details[1],
+                                                           host=client_db_details[4])
+                output_params = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+                
+                result_args, cursor = call_stored_procedure(client_db_connection, 
+                                                            'sproc_sec_login_v2', 
+                                                            login_id, 
+                                                            password,
+                                                            request.remote_addr, 
+                                                            "desktop", 
+                                                            token, *output_params)
+                
+                required_output_params = result_args[4:8]                
+                result_sets = sproc_response(cursor)
+                                
+                if result_args[10] == "Success":                
+                    result_sets_array = [result_set[0] for result_set in result_sets]
+
+                    close_connection(client_db_connection, cursor)
+
+                    result_json = {
+                        'token': token,
+                        'audit_screen_visit': required_output_params[0],
+                        'debug_sproc': required_output_params[1],
+                        'session_id': required_output_params[2],
+                        'user_id': required_output_params[3],
+                        'module_names': result_sets_array
+                    }
+                    return { 'Status': 'Success', 'Message': result_json}, 200
+                else:
+                    return { 'Status': 'Failure', 'Message': result_args[10]}, 400
         except Exception as error:
             return {"error_response": error}, 400
         
