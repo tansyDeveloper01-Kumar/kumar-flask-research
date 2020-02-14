0180d4b9cd9685a5314ed746b2ab8981ddca50f1
tansyDeveloper01 <gundla.kumar@tansycloud.com>
2020-02-14 15:27:05 +0530

    products

--- resources/db/executeSProc.py
+++ resources/db/executeSProc.py
@@ -15,3 +15,15 @@ def fn_sproc_response(cursor):
     for result in cursor.stored_results():
         return result.fetchall()
 
+# return function
+def fn_return_sproc_status(sproc_result_args, cursor, screen, functionality):
+    sproc_result_args_type = isinstance(sproc_result_args, str)
+    if sproc_result_args_type == True and cursor == 400:
+        return {'status': 'Failure', 'data': sproc_result_args}, 400
+    # sproc_result_args[5] = err_flag & sproc_result_args[7] = err_msg
+    elif sproc_result_args[5] == 1:
+        return {'status': 'Failure', 'data': sproc_result_args[7]}, 200
+    else:
+        sproc_result_sets = fn_sproc_response(cursor)
+
+        return {'status': 'Success', 'data': sproc_result_sets, 'message': screen+functionality+"Successfully"}, 200
--- resources/inventory/product.py
+++ resources/inventory/product.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from flask import request
 
-from resources.db.executeSProc import fn_call_stored_procedure, fn_sproc_response
+from resources.db.executeSProc import fn_call_stored_procedure, fn_sproc_response, fn_return_sproc_status
 from resources.utils.decorators.clientDBConnection import fn_make_client_db_connection
 from resources.utils.decorators.screenPermission import fn_check_screen_permission
 
@@ -80,17 +80,7 @@ class InvProduct(Resource):
                                                                  audit_screen_visit,
                                                                  *account_lkp_output_params)
 
-            sproc_result_args_type = isinstance(sproc_result_args, str)
-            if sproc_result_args_type == True and cursor == 400:
-                return {'status': 'Failure', 'data': sproc_result_args}, 400
-            # sproc_result_args[5] = err_flag & sproc_result_args[7] = err_msg
-            elif sproc_result_args[5] == 1:
-                return {'status': 'Failure', 'data': sproc_result_args[7]}, 200
-            else:
-                sproc_result_sets = fn_sproc_response(cursor)
-
-                return {'status': 'Success', 'data': sproc_result_sets}, 200
-
+            return fn_return_sproc_status(sproc_result_args, cursor, "Products ", "Fetched ")
         except Exception as e:
             return {'Error': str(e)}, 400
 
@@ -197,14 +187,6 @@ class InvProduct(Resource):
                                                                  audit_screen_visit,
                                                                  *output_params)
 
-            sproc_result_args_type = isinstance(sproc_result_args, str)
-            if sproc_result_args_type == True and cursor == 400:
-                return {'status': 'Failure', 'data': sproc_result_args}, 400
-            # sproc_result_args[5] = err_flag & sproc_result_args[7] = err_msg
-            elif sproc_result_args[5] == 1:
-                return {'status': 'Failure', 'data': sproc_result_args[7]}, 200
-            else:
-                return {'status': 'Success', 'result': "Product deleted successfully"}, 200
-
+            return fn_return_sproc_status(sproc_result_args, cursor, "Product ", "Deleted ")
         except Exception as e:
             return {'Error': str(e)}, 400
