8334bf498cf3949a66a7e0160cd6e63d39a0c34a
tansyDeveloper01 <gundla.kumar@tansycloud.com>
2020-02-19 15:13:25 +0530

    products details

--- resources/inventory/product.py
+++ resources/inventory/product.py
@@ -2,8 +2,8 @@ from flask_restful import Resource
 from flask import request
 
 from resources.db.executeSProc import fn_call_stored_procedure, fn_return_sproc_status, \
-                                      fn_return_sproc_single_result_sets, fn_return_sproc_ddl, \
-                                      fn_sproc_multiple_result_sets_response
+                                      fn_return_sproc_single_result_sets, fn_return_sproc_ddl,\
+                                      fn_sproc_multiple_result_sets_response, fn_get_sproc_errors
 from resources.utils.decorators.clientDBConnection import fn_make_client_db_connection
 from resources.utils.decorators.screenPermission import fn_check_screen_permission
 
@@ -14,30 +14,23 @@ class InvProductDetails(Resource):
     @fn_check_screen_permission()
     def get(self, *args, **kwargs):
         try:
-            entity_id = request.headers.get('entity_id')
-            debug_sproc = request.headers.get('debug_sproc')
-            audit_screen_visit = request.headers.get('audit_screen_visit')
+            entity_id = int(request.headers.get('entity_id'))
+            debug_sproc = int(request.headers.get('debug_sproc'))
+            audit_screen_visit = int(request.headers.get('audit_screen_visit'))
 
+            input_params = [entity_id, kwargs['session_id'], kwargs['user_id'],
+                            kwargs['screen_id'], debug_sproc, audit_screen_visit]
             output_params = [0, 0, 0]
             sproc_result_args, cursor = fn_call_stored_procedure(kwargs['client_db_connection'],
                                                                  'sproc_inv_product_detail',
-                                                                 int(entity_id),
-                                                                 kwargs['session_id'],
-                                                                 kwargs['user_id'],
-                                                                 kwargs['screen_id'],
-                                                                 debug_sproc,
-                                                                 audit_screen_visit,
+                                                                 *input_params,
                                                                  *output_params)
 
-            sproc_result_args_type = isinstance(sproc_result_args, str)
-            if sproc_result_args_type == True and cursor == 400:
-                return {'status': 'Failure', 'data': sproc_result_args}, 400
-            # sproc_result_args[5] = err_flag & sproc_result_args[7] = err_msg
-            elif sproc_result_args[5] == 1:
-                return {'status': 'Failure', 'data': sproc_result_args[7]}, 200
+            status, cursor_object, status_code = fn_get_sproc_errors(sproc_result_args=sproc_result_args, cursor=cursor)
+            if status == "Failure" and status_code == 400:
+                return {'status': status, 'data': cursor_object}, status_code
             else:
-                sproc_result_sets = fn_sproc_multiple_result_sets_response(cursor)
-
+                sproc_result_sets = fn_sproc_multiple_result_sets_response(cursor)[0]
                 product_details = [{'product': each_product[0],
                                     'product_type_entity_id': each_product[1],
                                     'unit_rate': str(each_product[2]),
